dbcfb8775f65070426b01e41f0d423a34a6c49c0
jiyeon <hannee233@dankook.ac.kr>
2025-05-24 22:45:09 +0900

    sign up almost

--- lib/signup.dart
+++ lib/signup.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
 
+  static const InputDecoration ButtonDecoration = InputDecoration(
+    enabledBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.orange)
+    ),
+    focusedBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.orange)
+    ),
+    hintStyle: TextStyle(color: Colors.orange),
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,7 +47,7 @@ class SignUp extends StatelessWidget {
                     textAlign: TextAlign.left,
                     style: TextStyle(
                       color: Colors.orange,
-                      fontSize: 20,
+                      fontSize: 22,
                     ),
                   ),
                 ],
@@ -49,24 +59,50 @@ class SignUp extends StatelessWidget {
               child: Column(
                 children: [
                   SizedBox(height: 50,),
+                  // 아이디 입력
                   TextField(
-                    decoration: InputDecoration(
-                      hintText: "아이디",
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.orange)
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.orange)
-                      )
-                    ),
+                    decoration: ButtonDecoration.copyWith(hintText: '아이디'),
                   ),
                   SizedBox(height: 14,),
+                  // 비밀번호 입력
                   TextField(
-                    decoration: InputDecoration(
-                      hintText: "비밀번호"
-                    ),
-                  )
+                    decoration: ButtonDecoration.copyWith(hintText: '비밀번호'),
+                  ),
+                  SizedBox(height: 14,),
+                  // 비밀번호 확인
+                  TextField(
+                    decoration: ButtonDecoration.copyWith(hintText: '비밀번호 확인'),
+                  ),
+                  SizedBox(height: 14,),
+                  // 이메일 입력
+                  TextField(
+                    decoration: ButtonDecoration.copyWith(hintText: '이메일'),
+                  ),
+                  SizedBox(height: 50,),
                 ],
+              ),
+            ),
+
+            GestureDetector(
+              onTap: () => {
+                print("회원가입 완료!")
+              },
+              child: Center(
+                child: Container(
+                  width: 330,
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Text(
+                      "계정생성하기",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
