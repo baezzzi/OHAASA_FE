6c87401810443b968b2fedcd2bd4e6eb6c4d0a72
jiyeon <hannee233@dankook.ac.kr>
2025-05-25 17:04:00 +0900

    design finish

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,111 +1,146 @@
 import 'package:flutter/material.dart';
+import 'style.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
-
-  static const InputDecoration ButtonDecoration = InputDecoration(
-    enabledBorder: UnderlineInputBorder(
-      borderSide: BorderSide(color: Colors.orange)
-    ),
-    focusedBorder: UnderlineInputBorder(
-      borderSide: BorderSide(color: Colors.orange)
-    ),
-    hintStyle: TextStyle(color: Colors.orange),
-  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-              onPressed: () => {
-                Navigator.pop(context)
-              }, icon: Icon(Icons.arrow_back_ios_new)),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 112,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Colors.orange,
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(50)
-                )
-              ),
-            ),
+        body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus(); // 키보드 숨기기
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
 
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50),
-              child: Column(
-                children: [
-                  SizedBox(height: 60),
-                  Text(
-                    "create\nyour account",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.orange,
-                      fontSize: 22,
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      height: 150,
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(50)
+                        )
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
+                    Positioned(
+                      top: 80,
+                      left: 15,
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back_ios_new, color: Colors.white),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
 
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 50),
-              child: Column(
-                children: [
-                  SizedBox(height: 50,),
-                  // 아이디 입력
-                  TextField(
-                    decoration: ButtonDecoration.copyWith(hintText: '아이디'),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 100),
+                      Text(
+                        "create\nyour account",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: Colors.orange,
+                          fontSize: 22,
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 14,),
-                  // 비밀번호 입력
-                  TextField(
-                    decoration: ButtonDecoration.copyWith(hintText: '비밀번호'),
-                  ),
-                  SizedBox(height: 14,),
-                  // 비밀번호 확인
-                  TextField(
-                    decoration: ButtonDecoration.copyWith(hintText: '비밀번호 확인'),
-                  ),
-                  SizedBox(height: 14,),
-                  // 이메일 입력
-                  TextField(
-                    decoration: ButtonDecoration.copyWith(hintText: '이메일'),
-                  ),
-                  SizedBox(height: 50,),
-                ],
-              ),
-            ),
+                ),
 
-            GestureDetector(
-              onTap: () => {
-                print("회원가입 완료!")
-              },
-              child: Center(
-                child: Container(
-                  width: 330,
-                  height: 50,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.orange,
-                    borderRadius: BorderRadius.circular(15),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 50,),
+                      Stack(
+                        children: [
+                          // 아이디 입력
+                          TextField(
+                            decoration: buttonDecoration.copyWith(hintText: '아이디'),
+                          ),
+                          Positioned(
+                            top: 10,
+                            right: 0,
+                            child: Container(
+                              width: 70,
+                              height: 30,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: Colors.orange,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Text(
+                                "중복확인",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 14,),
+                      // 비밀번호 입력
+                      TextField(
+                        decoration: buttonDecoration.copyWith(hintText: '비밀번호'),
+                        obscureText: true,
+                        keyboardType: TextInputType.visiblePassword,
+                      ),
+                      SizedBox(height: 14,),
+                      // 비밀번호 확인
+                      TextField(
+                        decoration: buttonDecoration.copyWith(hintText: '비밀번호 확인'),
+                        obscureText: true,
+                        keyboardType: TextInputType.visiblePassword,
+                      ),
+                      SizedBox(height: 14,),
+                      // 이메일 입력
+                      TextField(
+                        decoration: buttonDecoration.copyWith(hintText: '이메일'),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      SizedBox(height: 50,),
+                    ],
                   ),
-                  child: Text(
-                      "계정생성하기",
-                    style: TextStyle(
-                      color: Colors.white,
+                ),
+
+                GestureDetector(
+                  onTap: () => {
+                    print("회원가입 완료!")
+                  },
+                  child: Center(
+                    child: Container(
+                      width: 330,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Text(
+                          "계정생성하기",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
---
+++ lib/style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// 회원가입 입력 버튼 디자인
+const InputDecoration buttonDecoration = InputDecoration(
+  enabledBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.orange)
+  ),
+  focusedBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.orange)
+  ),
+  hintStyle: TextStyle(color: Colors.orange),
+);
