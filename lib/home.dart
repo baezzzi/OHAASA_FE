21be5422a80702f06601b8a62cb676ef6e6e1c2d
jiyeon <hannee233@dankook.ac.kr>
2025-05-29 05:39:21 +0900

    home ing

--- lib/home.dart
+++ lib/home.dart
@@ -30,35 +30,95 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-
-          child: Stack(
-            children: [
-              Container(
-              width: double.infinity,
-              height: 300,
-              decoration: BoxDecoration(
-                color: Colors.orange,
-                ),
-              ),
-              Align(
-                alignment: Alignment(0, -.2),
-                child: Container(
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: double.infinity,
+            height: 930,
+            child: Stack(
+              children: [
+                Container(
                   width: double.infinity,
-                  height: 500,
+                  height: 300,
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(60),
-                      topRight: Radius.circular(60),
+                    color: Colors.orange,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment(0, 1),
+                  child: Container(
+                    width: double.infinity,
+                    height: 800,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60),
+                      ),
+                    ),
+
+                    child: Column(
+                      children: [
+                        SizedBox(height: 90,),
+                        Text(
+                          "닉네임",
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 25,
+                          ),
+                        ),
+                        SizedBox(height: 5,),
+                        Container(
+                          width: 120,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "쌍둥이자리",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              )
-            ],
+                Align(
+                  alignment: Alignment(0, -.8),
+                  child: GestureDetector(
+                    onTap: _pickImage,
+
+                    child: Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(100),
+                        border: Border.all(
+                          width: 8,
+                          color: Colors.white,
+                        ),
+                        image: _imageFile != null
+                          ? DecorationImage(
+                          image: FileImage(_imageFile!),
+                          fit: BoxFit.cover,
+                        ) : null,
+                      ),
+                      child: _imageFile == null
+                        ? const Icon(Icons.face, color: Colors.white, size: 100,)
+                          : null,
+                    ),
+                  )
+                ),
+
+              ],
+            ),
           ),
         ),
       ),
