a24f749cb55367243d3c75c3d08b9349ece70f35
jiyeon <hannee233@dankook.ac.kr>
2025-06-18 17:40:11 +0900

    [ING] friend.dart header Container

--- lib/friendszodiac/friend.dart
+++ lib/friendszodiac/friend.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:OzO/layout/bottommenu.dart';
+import 'package:OzO/sidemenu/sidepopup.dart';
+
 class Friend extends StatefulWidget {
   const Friend({super.key});
 
@@ -11,7 +14,28 @@ class _FriendState extends State<Friend> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: SidePopup(),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 300,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [Colors.black54, Colors.blueGrey])
+            ),
+          ),
 
+          Text("data"),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding : EdgeInsets.only(bottom: 20),
+              child: BottomMenu(),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
